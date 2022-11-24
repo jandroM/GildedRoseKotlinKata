d42d54c30565275b4fb21e6a7f5ac3163128ca91
David Belenguer <david.belenguer@wallapop.com>
2022-11-24 16:01:51 +0100

    Added legendary item case

--- src/test/kotlin/com/gildedrose/GildedRoseTest.kt
+++ src/test/kotlin/com/gildedrose/GildedRoseTest.kt
@@ -7,6 +7,7 @@ import org.junit.jupiter.params.provider.MethodSource
 import java.util.stream.Stream
 
 const val AGED_BRIE = "Aged Brie"
+const val SULFURAS = "Sulfuras, Hand of Ragnaros"
 const val ELIXIR_OF_THE_MONGOOSE = "Elixir of the Mongoose"
 
 internal class GildedRoseTest {
@@ -14,21 +15,27 @@ internal class GildedRoseTest {
     companion object {
         @JvmStatic
         fun generateCases(): Stream<Arguments> = Stream.of(
-            Arguments.of(Item(AGED_BRIE, 2, 0), 1, 1),
-            Arguments.of(Item(ELIXIR_OF_THE_MONGOOSE, 5, 7), 4, 6),
-            Arguments.of(Item(ELIXIR_OF_THE_MONGOOSE, 0, 7), -1, 5),
-            Arguments.of(Item(ELIXIR_OF_THE_MONGOOSE, 3, 0), 2, 0),
-            Arguments.of(Item(AGED_BRIE, 3, 50), 2, 50)
+            Arguments.of("“Aged Brie” actually increases in Quality the older it gets", Item(AGED_BRIE, 2, 0), 1, 1),
+            Arguments.of("Normal item decreases quality by 1", Item(ELIXIR_OF_THE_MONGOOSE, 5, 7), 4, 6),
+            Arguments.of(
+                "Once the sell by date has passed, Quality degrades twice as fast",
+                Item(ELIXIR_OF_THE_MONGOOSE, 0, 7),
+                -1,
+                5
+            ),
+            Arguments.of("The Quality of an item is never negative", Item(ELIXIR_OF_THE_MONGOOSE, 3, 0), 2, 0),
+            Arguments.of("The Quality of an item is never more than 50", Item(AGED_BRIE, 3, 50), 2, 50),
+            Arguments.of("Sulfuras legendary item it's unalterable", Item(SULFURAS, 3, 50), 3, 50),
         )
     }
 
     @ParameterizedTest
     @MethodSource("generateCases")
-    fun test(item: Item, expectedSellIn: Int, expectedQuality: Int) {
+    fun test(case: String, item: Item, expectedSellIn: Int, expectedQuality: Int) {
         val app = GildedRose(listOf(item))
         app.updateQuality()
-        assertEquals(expectedSellIn, item.sellIn)
-        assertEquals(expectedQuality, item.quality)
+        assertEquals(expectedSellIn, item.sellIn, "$case expectedSellIn")
+        assertEquals(expectedQuality, item.quality, "$case expectedQuality")
     }
 }
 
--- src/test/kotlin/com/gildedrose/TexttestFixture.kt
+++ src/test/kotlin/com/gildedrose/TexttestFixture.kt
@@ -7,7 +7,7 @@ fun main(args: Array<String>) {
     val items = listOf(Item("+5 Dexterity Vest", 10, 20), //
             Item("Aged Brie", 2, 0), //
             Item("Elixir of the Mongoose", 5, 7), //
-            Item("Sulfuras, Hand of Ragnaros", 0, 80), //
+            Item("Sulfuras, Hand of Ragnaros", 2, 80), //
             Item("Sulfuras, Hand of Ragnaros", -1, 80),
             Item("Backstage passes to a TAFKAL80ETC concert", 15, 20),
             Item("Backstage passes to a TAFKAL80ETC concert", 10, 49),
@@ -17,7 +17,7 @@ fun main(args: Array<String>) {
 
     val app = GildedRose(items)
 
-    var days = 2
+    var days = 3
     if (args.size > 0) {
         days = Integer.parseInt(args[0]) + 1
     }

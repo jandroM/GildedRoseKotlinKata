f526c31bc1d444f1a39bab8e6a1760d011fcd1a0
Karla Astrid Quispe Ulloa <karla.quispe@wallapop.com>
2022-11-24 15:40:59 +0100

    elixir of the mongoose

--- src/test/kotlin/com/gildedrose/GildedRoseTest.kt
+++ src/test/kotlin/com/gildedrose/GildedRoseTest.kt
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 
 const val AGED_BRIE = "Aged Brie"
+const val ELIXIR_OF_THE_MONGOOSE = "Elixir of the Mongoose"
 
 internal class GildedRoseTest {
 
@@ -14,6 +15,15 @@ internal class GildedRoseTest {
         app.updateQuality()
         assertEquals(1, items[0].quality)
         assertEquals(1, items[0].sellIn)
+    }
+
+    @Test
+    fun `elixir of the mongoose should decrease in quality and sellIn`() {
+        val items = listOf(Item(ELIXIR_OF_THE_MONGOOSE, 5, 7))
+        val app = GildedRose(items)
+        app.updateQuality()
+        assertEquals(6, items[0].quality)
+        assertEquals(4, items[0].sellIn)
     }
 }
 

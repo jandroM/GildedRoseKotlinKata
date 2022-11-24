9ecf6e369053e18ead64cd98a81536a08269c181
Bartomeu Galmés <bartomeu.galmes@wallapop.com>
2022-11-24 15:38:10 +0100

    Decrease quality

--- src/test/kotlin/com/gildedrose/GildedRoseTest.kt
+++ src/test/kotlin/com/gildedrose/GildedRoseTest.kt
@@ -2,6 +2,7 @@ package com.gildedrose
 
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
+import java.io.File
 
 internal class GildedRoseTest {
 
@@ -10,8 +11,16 @@ internal class GildedRoseTest {
         val items = listOf(Item("foo", 0, 0))
         val app = GildedRose(items)
         app.updateQuality()
-        assertEquals("fixme", app.items[0].name)
+        assertEquals("foo", app.items[0].name)
 
+    }
+
+    @Test
+    fun reduces_quality_every_day(){
+        val items = listOf(Item("foo", 1, 1))
+        val app = GildedRose(items)
+        app.updateQuality()
+        assertEquals(0, app.items[0].quality)
     }
 
 }
